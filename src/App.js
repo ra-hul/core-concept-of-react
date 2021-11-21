3e201f8baa011498b284ee5477e0e20854622888
Rahul Paul <rahulshanto008@gmail.com>
2021-11-22 02:13:28 +0600

    commited

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
 
     </div>
   );
 }
 
-// will show a counter with a button if we increase that the number of counter will increase and counter value will decrease
-// second way , optimized
+// using the useEffect of react
+// using api of json
 
-function Counter() {
+// First of all we have to define the state of the react
+function ExternalUsers() {
+  // First of all we have to define the state of the react
+  const [users, setUsers] = useState([]);
 
-  const [count, setCount] = useState(10);
-  const handleIncrease = () => { setCount(count + 1) };
-  const handleDecrease = () => { setCount(count - 1) };
+  // For loading the data we are using useEffect along with annonymous function and empty array
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+
 
 
   return (
     <div >
-      <h1>Count: {count}</h1>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleDecrease}>Decrease</button>
+      <h2>External Users:</h2>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
     </div>
   );
 }
@@ -45,4 +54,12 @@ function Counter() {
 //     setCount(preCount);
 //   }
 
+// second way
+// const [count, setCount] = useState(10);
+// const handleIncrease = () => { setCount(count + 1) };
+// const handleDecrease = () => { setCount(count - 1) };
+// button dynamic section
+{/* <h1>Count: {count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button> */}
 export default App;
